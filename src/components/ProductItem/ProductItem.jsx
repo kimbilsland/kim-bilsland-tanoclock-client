cdef2ebe30aba45546e3283e1fe0199f0dea9ad9
Kimberly <kimbilsland@gmail.com>
2024-06-07 09:15:19 -0700

    update product photo gallery to display jpg photos

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -2,9 +2,12 @@ import "./ProductItem.scss";
 
 function VideoItem({ title, price, image, rating }) {
 
+const imagePath=`public/product-images/${image}`
+
+
   return (
     <>
-      <img src={image} alt={title}/>
+      <img className="product__image" src={imagePath} alt={title}/>
       <div>
         <h3>{title}</h3>
         <p>{rating}</p>
