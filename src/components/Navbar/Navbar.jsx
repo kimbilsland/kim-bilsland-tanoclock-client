3c37d488ada9951471b5436948389f6a512cf47a
Kimberly <kimbilsland@gmail.com>
2024-06-09 12:05:33 -0700

    add link to home feature on nav

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,17 @@
 import "./Navbar.scss";
 import navIcon from "../../assets/images/icons/menu.svg";
+import { Link } from "react-router-dom";
 
 function Nav() {
   return (
     <div className="navbar">
-      <h2 className="navbar__logo">TanO'Clock</h2>
-      <img src={navIcon} alt="navigation" />
+      <Link to="/">
+        <h2 className="navbar__logo">TanO'Clock</h2>
+      </Link>
+      <img className="navbar__icon" src={navIcon} alt="navigation" />
     </div>
   );
 }
 
 export default Nav;
+
